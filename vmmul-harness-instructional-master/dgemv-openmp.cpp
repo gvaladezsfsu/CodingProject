71e40ef5489a732e96d95b0079aba4344a3ee197
gvaladezsfsu <100495642+gvaladezsfsu@users.noreply.github.com>
2023-05-05 09:42:38 -0700

    Update dgemv-openmp.cpp

--- vmmul-harness-instructional-master/dgemv-openmp.cpp
+++ vmmul-harness-instructional-master/dgemv-openmp.cpp
@@ -24,6 +24,8 @@ void my_dgemv(int n, double* A, double* x, double* y) {
    // insert your dgemv code here. you may need to create additional parallel regions,
    // and you may want to comment out the above parallel code block that prints out
    // nthreads and thread_id so as to not taint your timings
-
+for (int i = 0; i < n; ++i) {
+        y[i] += A[i] * x[i];
+    }
 }
 
